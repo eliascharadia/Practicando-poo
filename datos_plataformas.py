ebc84439826fbf1981c1d102a9d3f973f1204495
eliascharadia <eliascharadia01@gmail.com>
2023-11-05 09:44:28 -0300

    Modificaciones generales

--- datos_plataformas.py
+++ datos_plataformas.py
@@ -2,6 +2,8 @@ import pygame
 from Configuraciones import crear_plataforma
 H, W = 500,1000
 plataformas = []
-piso = crear_plataforma(True, (W,20), 0, H-60, r"Recursos\3.png")
+piso = crear_plataforma(True, (219,20), 0, H-60, r"Recursos\3.png")
+piso2 = crear_plataforma(True, (219,20), 447, H-60, r"Recursos\3.png")
 
 plataformas.append(piso)
+plataformas.append(piso2)
